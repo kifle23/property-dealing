b53884dcf079c160f5a92651ee5731d2e89d642e
kifle23 <kiflekifle23@gmail.com>
2023-07-22 16:34:33 +0300

    Created UI and fetch data for property-card

--- Client/src/app/property/property-card/property-card.component.ts
+++ Client/src/app/property/property-card/property-card.component.ts
@@ -5,11 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './property-card.component.html',
   styleUrls: ['./property-card.component.css']
 })
-export class PropertyCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class PropertyCardComponent {
+  Property: any = {
+    "Id": 1,
+    "Name": "Birla House",
+    "Type": "House",
+    "Price": 12000
+}
 }
