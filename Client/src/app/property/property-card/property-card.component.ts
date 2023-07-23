dfbd3879b4c1d3cf3d9a26d6d8011af28ba95cdd
kifle23 <kiflekifle23@gmail.com>
2023-07-23 10:30:12 +0300

    Setup property list
    Pass data from list to card using input decorator
    
    Use ngFore structural directive
    
    Use property binding

--- Client/src/app/property/property-card/property-card.component.ts
+++ Client/src/app/property/property-card/property-card.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
   templateUrl: './property-card.component.html',
-  styleUrls: ['./property-card.component.css']
+  styleUrls: ['./property-card.component.css'],
 })
 export class PropertyCardComponent {
-  Property: any = {
-    "Id": 1,
-    "Name": "Birla House",
-    "Type": "House",
-    "Price": 12000
-}
+  @Input() property: any;
+  
 }
