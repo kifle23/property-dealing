de42020873c6f4fe9bd267106bfa44353927de45
kifle23 <kiflekifle23@gmail.com>
2023-07-25 19:26:00 +0300

    Use template driven forms for add property component

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { PropertyListComponent } from './property/property-list/property-list.co
 import { HousingService } from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+import { FormsModule } from '@angular/forms'; 
 
 const appRoutes: Routes = [
   {path: '', component: PropertyListComponent},
@@ -31,7 +32,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [
     HousingService
