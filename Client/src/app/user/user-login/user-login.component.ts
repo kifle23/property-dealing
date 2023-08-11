4d1224216cbcf1868a8c2e54901806b6dd7afd26
kifle23 <kiflekifle23@gmail.com>
2023-08-11 11:37:32 +0300

    Add UI for user login component

--- Client/src/app/user/user-login/user-login.component.ts
+++ Client/src/app/user/user-login/user-login.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.css']
+  styleUrls: ['./user-login.component.css'],
 })
 export class UserLoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onLogin(loginForm: NgForm) {
+    console.log(loginForm.value);
   }
-
 }
