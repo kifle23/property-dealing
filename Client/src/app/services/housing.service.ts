62de25fd3a849c25fde3b9deecbd1097fbbb8592
kifle23 <kiflekifle23@gmail.com>
2023-08-19 14:16:07 +0300

    Save new property to local storage

--- Client/src/app/services/housing.service.ts
+++ Client/src/app/services/housing.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs/internal/Observable';
 import { Ipropertybase } from '../model/ipropertybase';
+import { Property } from '../model/property';
 
 interface PropertyData {
   [key: string]: any;
@@ -27,5 +28,9 @@ export class HousingService {
           return propertiesArray;
         })  
     );
+  }
+
+  addProperty(property: Property) {
+    localStorage.setItem('newProp', JSON.stringify(property));
   }
 }
