62fe50e889f0f5db8d58a75abcc8970c70b578fa
kifle23 <kiflekifle23@gmail.com>
2023-08-22 18:21:38 +0300

    Setup connection to database

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using webapi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
+builder.Services.AddDbContext<DBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
