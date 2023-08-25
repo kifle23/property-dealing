286946098785868acdde0a7a93b62cf39fbca81a
kifle23 <kiflekifle23@gmail.com>
2023-08-25 10:00:27 +0300

    Update City controller to use repository

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using webapi.Data;
+using webapi.Data.Repo;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ICityRepository, CityRepository>();
 
 builder.Services.AddCors(options =>
 {
