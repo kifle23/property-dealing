21b7d8b4c45c77cbf7cbfe8326acd7346b9b12df
kifle23 <kiflekifle23@gmail.com>
2023-08-26 14:52:01 +0300

    Add columns to City

---
+++ webapi/Migrations/20230826114823_AddLastUpdatedFields.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastUpdatedFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LastUpdatedBy",
+                table: "Cities",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdatedOn",
+                table: "Cities",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedBy",
+                table: "Cities");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedOn",
+                table: "Cities");
+        }
+    }
+}
--- webapi/Models/City.cs
+++ webapi/Models/City.cs
@@ -4,5 +4,8 @@ namespace webapi.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public DateTime LastUpdatedOn { get; set; }
+
+        public int LastUpdatedBy { get; set; }
     }
 }
