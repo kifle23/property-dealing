31d36ff49d29aa441bc7303102a762d07ccd7356
kifle23 <kiflekifle23@gmail.com>
2023-09-04 20:55:33 +0300

    Input validation

--- webapi/Dtos/CityDto.cs
+++ webapi/Dtos/CityDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace webapi.Dtos
     public class CityDto
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Name must be alphabetic")]
         public string Name { get; set; }
+        [Required]
         public string Country { get; set; }
         
     }
