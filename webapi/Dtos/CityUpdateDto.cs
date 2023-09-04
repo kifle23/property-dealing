859300fdc727633a42e224b5096214e293cef25c
kifle23 <kiflekifle23@gmail.com>
2023-09-04 09:30:25 +0300

    Create City Update DTO

---
+++ webapi/Dtos/CityUpdateDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi.Dtos
+{
+    public class CityUpdateDto
+    {
+        public string Name { get; set; }
+        
+    }
+}
