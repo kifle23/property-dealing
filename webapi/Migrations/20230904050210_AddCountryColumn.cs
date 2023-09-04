97b31fd15f165c8241abcb53448b5ce1d16bf055
kifle23 <kiflekifle23@gmail.com>
2023-09-04 08:04:24 +0300

    Add migrations for column country

---
+++ webapi/Migrations/20230904050210_AddCountryColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCountryColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Cities",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Cities");
+        }
+    }
+}
