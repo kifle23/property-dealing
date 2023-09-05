0dac8893cf3a1361a86f2ed2af6b974b6234ba41
kifle23 <kiflekifle23@gmail.com>
2023-09-05 08:25:54 +0300

    Modify error hundler to hundle multiple errors

--- webapi/Extentions/ExceptionMiddlewareExtentions.cs
+++ webapi/Extentions/ExceptionMiddlewareExtentions.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Diagnostics;
+using webapi.Middlewares;
 
 namespace webapi.Extentions
 {
     public static class ExceptionMiddlewareExtentions
     {
         public static void ConfigureExceptionHandler(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+        public static void ConfigureBuiltinExceptionHandler(this IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -18,9 +23,11 @@ namespace webapi.Extentions
             else
             {
                 app.UseExceptionHandler(
-                    options => {
+                    options =>
+                    {
                         options.Run(
-                            async context => {
+                            async context =>
+                            {
                                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                                 var ex = context.Features.Get<IExceptionHandlerFeature>();
                                 if (ex != null)
--- webapi/Middlewares/ExceptionMiddleware.cs
+++ webapi/Middlewares/ExceptionMiddleware.cs
@@ -31,14 +31,28 @@ namespace webapi.Middlewares
             catch (Exception ex)
             {
                 APIError response;
-                int statusCode = (int)HttpStatusCode.InternalServerError;
+                int statusCode;
+                string message;
+                var exceptionType = ex.GetType();
+
+                if (exceptionType == typeof(UnauthorizedAccessException))
+                {
+                    statusCode = (int)HttpStatusCode.Forbidden;
+                    message = "You are not authorized";
+                }
+                else
+                {
+                    statusCode = (int)HttpStatusCode.InternalServerError;
+                    message = "Internal Server Error";
+                }
+                
                 if (env.IsDevelopment())
                 {
                     response = new APIError((int)statusCode, ex.Message, ex.StackTrace?.ToString());
                 }
                 else
                 {
-                    response = new APIError(statusCode, ex.Message);
+                    response = new APIError(statusCode, message);
                 }
                 logger.LogError(ex, ex.Message);
                 context.Response.StatusCode = statusCode;
