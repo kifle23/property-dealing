ff5e48f933603fd7bbd974e7aa836c3d30e93adb
kifle23 <kiflekifle23@gmail.com>
2023-09-05 21:34:10 +0300

    Add model user

---
+++ webapi/Models/User.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
