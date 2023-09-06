d44d498753e22fbaf443c94c52117b905d6dc9b0
kifle23 <kiflekifle23@gmail.com>
2023-09-06 08:52:46 +0300

    Add migration for user model

---
+++ webapi/Migrations/20230906054321_UpdateUserPasswordEncryption.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserPasswordEncryption : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Password",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: "password");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordKey",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordKey",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)");
+        }
+    }
+}
