4194f3444139baff1404449341ca2ed8468d981d
kifle23 <kiflekifle23@gmail.com>
2023-09-06 07:04:43 +0300

    Generate JWT on successful authentication

--- webapi/Controllers/AccountController.cs
+++ webapi/Controllers/AccountController.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using webapi.Dtos;
 using webapi.Interfaces;
+using webapi.Models;
 
 namespace webapi.Controllers
 {
@@ -30,9 +33,31 @@ namespace webapi.Controllers
             var loginRes = new LoginResDto
             {
                 Username = result.Username,
-                Token = "Token to be generated"
+                Token = GenerateToken(result)
             };
             return Ok(loginRes);
+        }
+
+        private string GenerateToken(User user)
+        {
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("super secret key"));
+            var claims = new Claim[] {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+             };
+            var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = "issuer",
+                Audience = "audience",
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddMinutes(1),
+                SigningCredentials = signingCredentials
+            };
+            var tokenHandler = new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
