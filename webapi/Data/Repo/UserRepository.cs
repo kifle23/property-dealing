84e8cfed1ab3843723ddbd835443a7377d8bbb45
kifle23 <kiflekifle23@gmail.com>
2023-09-06 09:01:55 +0300

    Create user registartion in data store

--- webapi/Data/Repo/UserRepository.cs
+++ webapi/Data/Repo/UserRepository.cs
@@ -13,11 +13,38 @@ namespace webapi.Data.Repo
         public DBContext Context { get; }
         public UserRepository(DBContext context)
         {
-            this.Context = context;            
+            this.Context = context;
         }
         public async Task<User> Authenticate(string username, string password)
         {
-            return await Context.Users.FirstOrDefaultAsync(x => x.Username == username && x.Password == password);
+            return await Context.Users.FirstOrDefaultAsync(x => x.Username == username
+            // && x.Password == password
+            );
+        }
+
+        public void Register(string username, string password)
+        {
+            byte[] passwordHash, passwordKey;
+
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordKey = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+
+            var user = new User
+            {
+                Username = username,
+                Password = passwordHash,
+                PasswordKey = passwordKey
+            };
+
+            Context.Users.Add(user);
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            return await Context.Users.AnyAsync(x => x.Username == username);
         }
     }
 }
--- webapi/Interfaces/IUserRepository.cs
+++ webapi/Interfaces/IUserRepository.cs
@@ -9,5 +9,9 @@ namespace webapi.Interfaces
     public interface IUserRepository
     {
         Task<User> Authenticate(string username, string password);
+
+        void Register(string username, string password);
+
+        Task<bool> UserExists(string username);
     }
 }
