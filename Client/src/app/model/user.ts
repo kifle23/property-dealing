bdaf8346ab3b233b637d497040c08abf34786584
kifle23 <kiflekifle23@gmail.com>
2023-09-08 20:19:39 +0300

    Create client DTO for user

--- Client/src/app/model/user.ts
+++ Client/src/app/model/user.ts
@@ -1,6 +1,12 @@
 export interface User {
-    userName: string;
-    email?: string;
-    password: string;
-    mobile?: number;
+  userName: string;
+  email?: string;
+  password: string;
+  mobile?: number;
+}
+
+export interface UserForLogin {
+  username: string;
+  password: string;
+  token: string;
 }
