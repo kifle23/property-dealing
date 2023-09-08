483c2f34c6ec5313d318db6f0c1d3aec25170610
kifle23 <kiflekifle23@gmail.com>
2023-09-08 20:58:42 +0300

    Consume api to register new user

--- Client/src/app/services/auth.service.ts
+++ Client/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { UserForLogin } from '../model/user';
+import { UserForLogin, UserForRegistration } from '../model/user';
 
 @Injectable({
   providedIn: 'root',
@@ -12,5 +12,9 @@ export class AuthService {
 
   authUser(user: UserForLogin) {
     return this.http.post<any>(this.baseUrl + '/account/login', user);
+  }
+
+  registerUser(user: UserForRegistration) {
+    return this.http.post<any>(this.baseUrl + '/account/register', user);
   }
 }
